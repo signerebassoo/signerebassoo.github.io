b2d79cc5db9fd8ffb1445a4107bd154b322ecb62
signerebassoo <signe.rebassoo@hotmail.com>
2022-10-17 23:04:59 +0100

    Colour scheme change

--- src/NavBar/NavLinks/NavButton/NavButton.tsx
+++ src/NavBar/NavLinks/NavButton/NavButton.tsx
@@ -7,7 +7,7 @@ interface NavButtonProps {
 
 export default function NavButton(props: NavButtonProps) {
   return (
-    <a className='bg-gradient-to-br from-indigo-500 to-purple-500 text-white px-4 pt-2 pb-1 ml-8 rounded-lg' 
+    <a className='bg-gradient-to-br from-teal-500 to-cyan-500 text-white px-4 pt-2 pb-1 ml-8 rounded-lg' 
       href={props.link}>
       {props.name}
     </a>
