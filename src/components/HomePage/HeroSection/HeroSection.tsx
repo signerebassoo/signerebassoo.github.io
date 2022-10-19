3f02629da1cfcaa91e651fd1bce1645897a8d22b
signerebassoo <signe.rebassoo@hotmail.com>
2022-10-19 01:01:29 +0100

    Use zinc tones

--- src/components/HomePage/HeroSection/HeroSection.tsx
+++ src/components/HomePage/HeroSection/HeroSection.tsx
@@ -21,7 +21,7 @@ function HeroText() {
     <div className='text-center p-10'>
       <h2 className='sm:text-6xl text-4xl py-2 text-teal-600 font-medium'>{'<Signe Rebassoo />'}</h2>
       <h3 className='sm:text-3xl text-xl py-2 sm:font-normal font-medium'>Software developer, product engineer.</h3>
-      <p className='sm:text-lg text-base py-5 leading-8 text-gray-800 max-w-xl mx-auto'>Full-time back-end developer in fintech. Open to join forces for innovative and challenging side-projects. Let's connect!</p>
+      <p className='sm:text-lg text-base py-5 leading-8 text-zinc-800 max-w-xl mx-auto'>Full-time back-end developer in fintech. Open to join forces for innovative and challenging side-projects. Let's connect!</p>
     </div>
   );
 }
--- src/components/HomePage/HeroSection/Socials/Socials.tsx
+++ src/components/HomePage/HeroSection/Socials/Socials.tsx
@@ -3,7 +3,7 @@ import { AiFillLinkedin, AiFillGithub, AiFillTwitterCircle } from 'react-icons/a
 
 export default function Socials() {
   return (
-    <div className='sm:text-5xl text-4xl flex justify-center gap-16 py-3 text-gray-600'>
+    <div className='sm:text-5xl text-4xl flex justify-center gap-16 py-3 text-zinc-600'>
       <a href='https://www.linkedin.com/in/signe-rebassoo/' target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></a>
       <a href='https://github.com/signerebassoo' target="_blank" rel="noopener noreferrer"><AiFillGithub /></a>
       <a href='/'><AiFillTwitterCircle /></a>
